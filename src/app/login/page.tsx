ddd2274e921829806fd62e140105510cfa65c36b
aschechter10 <aschechter03@gmail.com>
2025-02-23 17:50:14 -0500

    removed twitter login (not working)

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,19 +1,8 @@
 "use client";
 
 import GoogleAuthButton from "@/components/auth/GoogleAuthButton";
-import TwitterAuthButton from "@/components/auth/TwitterAuthButton";
-import { useSession } from "next-auth/react";
 
 export default function Login() {
-  const { data: session, status } = useSession();
-  if (!session || status.toString() === "loading") {
-    console.log("not signed in");
-  } else {
-    console.log("signed in");
-    console.log("username", session.user?.name);
-    console.log("email", session.user?.email);
-  }
-
   return (
     <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
       <div className="w-full rounded-lg shadow border md:mt-0 sm:max-w-md xl:p-0 bg-neutral-900 border-neutral-800">
@@ -23,7 +12,6 @@ export default function Login() {
           </h1>
           <div className="space-y-4 md:space-y-6 w-full">
             <GoogleAuthButton />
-            <TwitterAuthButton />
           </div>
         </div>
       </div>
