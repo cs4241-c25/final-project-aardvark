c76424f2cbc725d1c888c2b9c77efb8a0e7f3c78
aschechter10 <aschechter03@gmail.com>
2025-02-24 20:12:37 -0500

    nope didnt work

--- src/app/api/consensi/[consensusNum]/route.ts
+++ src/app/api/consensi/[consensusNum]/route.ts
@@ -1,15 +1,18 @@
 import { Consensi } from "@/lib/datalayer";
 import { getServerSession } from "next-auth";
 
-export async function GET(request: Request, context: { consensusNum: number }) {
+export async function GET(
+  request: Request,
+  context: { params: { consensusNum: number } }
+) {
   // return one consensus by consensusNum
   const session = await getServerSession();
 
   if (!session) {
     return Response.json({ error: "Unauthorized" }, { status: 401 });
   }
-  const params = await context;
-  const consensusNum = context.consensusNum;
+  const params = await context.params;
+  const consensusNum = params.consensusNum;
   const consensiDataLayer = new Consensi();
   const consensi = await consensiDataLayer.getTodaysConsensiByNum(consensusNum);
   return Response.json({ consensi });
