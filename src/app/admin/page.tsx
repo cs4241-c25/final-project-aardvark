2f77b1528840ab5cc7d5a767c63a70bedc70e63d
aschechter10 <aschechter03@gmail.com>
2025-03-06 10:03:40 -0500

    more ui fixes to admin page

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -3,23 +3,13 @@ import ConsensiManager from "@/components/ConsensiManager";
 import GameHeader from "@/components/GameHeader";
 import LoadingSpinner from "@/components/LoadingSpinner"; // Import spinner icon
 import ModalWrapper from "@/components/ModalWrapper";
-import { Button } from "@/components/ui/Button";
-import { Textarea } from "@/components/ui/textarea";
 import { ModalProvider } from "@/context/ModalContext";
-import { ConsensiSuggestion } from "@/lib/interfaces";
 import axios from "axios";
-import { Loader2 } from "lucide-react";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
 export default function manageConsensi() {
-  const [data, setData] = useState<ConsensiSuggestion[]>([]);
-
-  const [userPrompt, setUserPrompt] = useState<String>("");
-
-  const [loading, setLoading] = useState<boolean>(false);
-
   const { status } = useSession();
 
   const [userAuthLoad, setUserAuthLoad] = useState(true);
@@ -44,18 +34,6 @@ export default function manageConsensi() {
     checkAdmin();
   }, [status]);
 
-  async function sendPrompt() {
-    setLoading(true);
-    try {
-      const response = await axios.post("/api/gemini", userPrompt);
-      setData(JSON.parse(response.data));
-    } catch (error) {
-      console.error("Error fetching data:", error);
-    } finally {
-      setLoading(false);
-    }
-  }
-
   return (
     <>
       <ModalProvider>
@@ -75,22 +53,7 @@ export default function manageConsensi() {
               your own using ai. See the current Consensus queue and create your
               own.
             </p>
-            <ConsensiManager aiSuggestions={data}></ConsensiManager>
-            {loading && (
-              <div className="flex justify-center mt-4">
-                <Loader2 className="animate-spin text-white h-8 w-8" />
-              </div>
-            )}
-            <p className="m-5">
-              Enter a category in the box below to generate new consensus
-              options.
-            </p>
-            <Textarea
-              placeholder="Enter your desired category"
-              onChange={(e) => setUserPrompt(e.target.value)}
-              className="border border-white text-white bg-transparent m-2 w-[600px] rounded-md mb-4"
-            ></Textarea>
-            <Button onClick={sendPrompt}>Generate</Button>
+            <ConsensiManager></ConsensiManager>
           </div>
         )}
       </ModalProvider>
