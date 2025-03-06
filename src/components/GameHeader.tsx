7ae2e6bd2e18f033070a6c76110fa4ccbeb92121
Ari Schechter <43043654+aschechter10@users.noreply.github.com>
2025-03-05 23:53:28 -0500

    Update GameHeader.tsx

--- src/components/GameHeader.tsx
+++ src/components/GameHeader.tsx
@@ -20,39 +20,45 @@ export default function GameHeader() {
   const { data: session } = useSession();
 
   return (
-      <header className="flex px-2 md:px-4 border-b border-inset py-2">
-        <div className="md:flex items-center">
-          <h1 className="font-funnel font-black text-2xl md:text-4xl md:border-r pr-4">
-            Consensus
-          </h1>
-          <p className="md:text-base text-sm md:ml-4 font-funnel">
-            #{formattedConsensusNum(Number(consensusTheme?.consensusNum))} -{" "}
-            {consensusTheme?.category}
-          </p>
+    <header className="flex px-2 md:px-4 border-b border-inset py-2">
+      <div className="md:flex items-center">
+        <h1 className="font-funnel font-black text-2xl md:text-4xl md:border-r pr-4">
+          Consensus
+        </h1>
+        <p className="md:text-base text-sm md:ml-4 font-funnel">
+          #{formattedConsensusNum(Number(consensusTheme?.consensusNum))} -{" "}
+          {consensusTheme?.category}
+        </p>
+      </div>
+      <ModalProvider>
+        <div className="flex flex-grow justify-end items-center gap-2">
+          <IconButton
+            title="Archive"
+            icon={<ClockCounterClockwise size={24} />}
+          />
+          {/* goes to consensus archive- TODO: create page */}
+          {/* <IconButton
+            title="Today's Statistics"
+            icon={<ChartBar size={24} />}
+          /> */}
+          {/* goes to statistics for today's consensus- TODO: create page */}
+          <IconButton
+            title="How to Play"
+            icon={<Question size={24} />}
+            onClick={() => openModal("How to Play")}
+          />
+          {/* opens how to play modal- TODO: create modal */}
+          {session?.user?.image !== "anonymous" ? (
+            <IconButton
+              title="My Profile"
+              icon={<UserCircle size={24}
+              onClick={() => router.push("/profile")}
+            />} />
+          ) : (
+            <Button variant="secondary" className="ml-1" onClick={() => router.push("/login")}>Log In</Button>
+          )}
         </div>
-        <ModalProvider>
-          <div className="flex flex-grow justify-end items-center gap-1">
-            <IconButton
-                title="Archive"
-                icon={<ClockCounterClockwise size={24} />}
-                onClick={() => router.push("/archive")}
-            />
-            {/* goes to consensus archive */}
-            <IconButton
-                title="Today's Statistics"
-                icon={<ChartBar size={24} />}
-            />
-            {/* goes to statistics for today's consensus */}
-            <IconButton
-                title="How to Play"
-                icon={<Question size={24} />}
-                onClick={() => openModal("How to Play")}
-            />
-            {/* opens how to play modal */}
-            <IconButton title="My Profile" icon={<UserCircle size={24} />} />
-            {/* goes to user profile page */}
-          </div>
-        </ModalProvider>
-      </header>
+      </ModalProvider>
+    </header>
   );
 }
