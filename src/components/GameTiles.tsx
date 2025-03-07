568755c20891b0c6ca9eef0e8adc979b3ba29e3a
Gustave Montana <77447364+gnonk323@users.noreply.github.com>
2025-03-07 15:00:20 -0500

    tile animation changes

--- src/components/GameTiles.tsx
+++ src/components/GameTiles.tsx
@@ -12,9 +12,10 @@ function WordBankTile({ tile, handleClick }: WordBankTileProps) {
   const { bgColorMap } = useGameContext();
   return (
     <motion.button
+      whileHover={{ scale: 1.02 }}
+      whileTap={{ scale: 0.95 }}
       initial={{ scale: 0.8, opacity: 0 }}
       animate={{ scale: 1, opacity: 1 }}
-      transition={{ type: "spring", stiffness: 300, damping: 15 }}
       className={clsx(
         "h-10 md:h-12 rounded text-[#0a0a0a] uppercase font-bold font-funnel md:text-xl",
         bgColorMap.get(tile.color),
@@ -41,11 +42,13 @@ function RankedTile({
   const { bgColorMap } = useGameContext();
   return (
     <motion.button
+      whileHover={{ scale: 1.02 }}
+      whileTap={{ scale: 0.95 }}
       initial={{ scale: 0.8, opacity: 0 }}
       animate={{
         scale: 1,
         opacity: 1,
-        ...(animateOnSubmit && { scale: [1, 1.1] }), // Pulse effect on submit
+        ...(animateOnSubmit && { scale: [1, 1.1] }),
       }}
       transition={{ type: "spring", stiffness: 300, damping: 15 }}
       className={clsx(
@@ -74,8 +77,8 @@ function EmptyRankedTile({
   return (
     <button
       className={clsx(
-        "h-12 md:h-16 rounded border-2 hover:border-4 transition-all bg-inset uppercase font-bold text-black/40 dark:text-white/40",
-        currentDestination === id ? "border-4 dark:bg-white/30 bg-black/20" : ""
+        "h-12 md:h-16 rounded border-2 hover:border-4 transition-all dark:border-white/40 border-black/40 uppercase font-bold text-black/40 dark:text-white/40",
+        currentDestination === id ? "border-4 dark:bg-white/20 bg-black/20" : ""
       )}
       onClick={() => id !== undefined && handleClick(id)}
     >
